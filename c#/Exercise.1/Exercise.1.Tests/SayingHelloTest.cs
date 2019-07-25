667173475f1fb182ee804d33fff621a69b8a7467
Sid S. Ryu <sid@sidryu.com>
2019-07-25 17:05:19 +0900

    프로젝트 시작
    - 연습문제 1번 시작
    - NUnit 테스트 시작
    - .gitignore 추가

---
+++ c#/Exercise.1/Exercise.1.Tests/SayingHelloTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise._1.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class SayingHelloTest
+    {
+        [Test]
+        public void Say_Default_ReturnHello()
+        {
+            string hello = SayingHello.Say();
+            Assert.That(hello, Is.EqualTo("hello"));
+        }
+    }
+}
---
+++ c#/Exercise.1/Exercise.1/SayingHello.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise._1
+{
+    public class SayingHello
+    {
+        public static string Say()
+        {
+            return "hello";
+        }
+    }
+}
